a2e20b79c1807ba581ecedb04203b20db9fbf05b
mohmed5597 <136842975+mohmed5597@users.noreply.github.com>
2026-08-08 22:33:49 +0300

    ver 0.1

--- project_clinic_doctors.sql
+++ project_clinic_doctors.sql
@@ -3,7 +3,7 @@
 -- Host: 127.0.0.1    Database: project_clinic
 -- ------------------------------------------------------
 -- Server version	9.7.1
-
+--gklblhvg
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
